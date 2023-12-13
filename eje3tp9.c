b54baf282627ab98a501cc290cae99575b01fb7f
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-12-12 23:50:37 -0300

    agregue el modulo para listar pedidos

--- eje3tp9.c
+++ eje3tp9.c
@@ -1,47 +1,47 @@
-//Sembrando Aire
+// Sembrando Aire
 
-#include <stdio.h>
 #include <ctype.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 
 //________________ Declaracion de estructuras _________________
 
-typedef struct{
+typedef struct {
     char nombreCientifico[15];
     char categoria[15];
     char nombreVulgar[15];
-}DatosEspecie;
+} DatosEspecie;
 
-typedef struct{
+typedef struct {
     char razonSocial[15];
     long int telefono;
-}DatosCliente;
+} DatosCliente;
 
-typedef struct{
+typedef struct {
     char apellidoEmpleado[15];
     char nombreEmpleado[15];
-}DatosEmpleado;
+} DatosEmpleado;
 
-typedef struct{
+typedef struct {
     DatosCliente datosCliente;
     DatosEmpleado datosEmpleado;
     int cantidad;
-}DatosPedido;
+} DatosPedido;
 
-typedef struct{
+typedef struct {
     DatosEspecie especie;
     int stock;
     float precio;
     int cantPedidos;
     DatosPedido *pedido;
-}DatosArbol;
+} DatosArbol;
 
 //_____________________________________________________________
 
 //_______________ Declaracion de funciones ____________________
 
 void mostrarMenu();
-void mostrarMenu(){
+void mostrarMenu() {
     printf("______ Menu ______\n");
     printf("\n");
     printf("01. Mostrar todos los arboles/especies disponibles\n");
@@ -50,17 +50,16 @@ void mostrarMenu(){
     printf("04. Agregar pedidos a una especie\n");
     printf("05. Actualizar stock de una especie\n");
     printf("06. Actualizar el precio de una especie\n");
-    printf("07. Salir\n"); 
+    printf("07. Salir\n");
     printf("_____________________\n");
 }
 
 void mostrarTodosLosArboles(DatosArbol *arbolito, int cant);
-void mostrarTodosLosArboles(DatosArbol *arbolito, int cant){    
+void mostrarTodosLosArboles(DatosArbol *arbolito, int cant) {
     printf("______ Lista de arboles ______\n");
     printf("\n");
-    for (int i = 0; i < cant; i++)
-    {
-        printf("__ Arbol: %d __\n", i+1);
+    for (int i = 0; i < cant; i++) {
+        printf("__ Arbol: %d __\n", i + 1);
         printf("\n");
         printf("Nombre cientifico: %s\n", arbolito[i].especie.nombreCientifico);
         printf("Categoria: %s\n", arbolito[i].especie.categoria);
@@ -73,37 +72,69 @@ void mostrarTodosLosArboles(DatosArbol *arbolito, int cant){
     printf("____________________________\n");
 }
 
+void mostrarSegunStock(DatosArbol *arbolito, int stock, int cant);
+void mostrarSegunStock(DatosArbol *arbolito, int stock, int cant) {
+    for (int i = 0; i < cant; i++) {
+        if (arbolito[i].stock < stock) {
+            mostrarTodosLosArboles(arbolito, 1);
+        }
+    }
+}
+
+void mostrarTodosLosPedidos(DatosArbol *arbolito, int cant);
+void mostrarTodosLosPedidos(DatosArbol *arbolito, int cant) {
+    for (int i = 0; i < cant; i++) {
+        for (int j = 0; j < arbolito[i].cantPedidos; j++) {
+            float montoTotal = arbolito[i].cantPedidos * arbolito[i].precio;
+            printf("_______________\n");
+            printf("\n");
+            printf("Razon social: %s\n", arbolito[i].pedido[j].datosCliente.razonSocial);
+            printf("Telefono: %d\n", arbolito[i].pedido[j].datosCliente.telefono);
+            printf("Cantidad de pedidos: %ld\n", arbolito[i].cantPedidos);
+            printf("Total a pagar: $%2.f\n", montoTotal);
+            printf("_______________\n");
+        }
+    }
+}
+
 //_____________________________________________________________
 
 //_________________ Funcion principal _________________________
 
-int main(){
-    //mostrarMenu();
+int main() {
+    // mostrarMenu();
     DatosArbol arboles[] = {
-        {
-            {"Catalpa", "Caduca", "Arce Rojo"},
-            8,
-            65000,
-            3,
-            NULL
-        },
-        {
-            {"Betula", "Perenne", "Olivo"},
-            9,
-            80000,
-            7,
-            NULL
-        },
-        {
-            {"Tilia", "Conifera", "Abeto"},
-            3,
-            45000,
-            6,
-            NULL
-        }
-    };
+        {{"Catalpa", "Caduca", "Arce Rojo"},
+         8,
+         65000,
+         0,
+         NULL},
+        {{"Betula", "Perenne", "Olivo"},
+         9,
+         80000,
+         0,
+         NULL},
+        {{"Tilia", "Conifera", "Abeto"},
+         3,
+         45000,
+         0,
+         NULL}};
     DatosArbol *listaArboles;
     listaArboles = arboles;
-    mostrarTodosLosArboles(listaArboles, 3);
+    mostrarTodosLosPedidos(listaArboles, 3);
+    // Asigno el tamaño en funcion de la cant de clientes que hayan realizado pedidos.
+    /*for (int i = 0; i < 3; i++) {
+        listaArboles[i].pedido = (DatosPedido *)malloc(sizeof(DatosPedido) * listaArboles[i].cantPedidos);
+
+        for (int j = 0; j < listaArboles[i].cantPedidos; j++)
+        {
+
+
+        }
+
+    }*/
+
+    // mostrarTodosLosArboles(listaArboles, 3);
+    // mostrarSegunStock(listaArboles,5,3);
     return 0;
 }
